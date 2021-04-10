7739b6de094980e94e393dbaf5b785a070ce62ba
Mostafa Waleed <mw18200615@gmail.com>
2021-04-10 21:05:47 +0300

    test

---
+++ app.js
@@ -0,0 +1,6 @@
+const hamMenu = document.getElementById("hamMenu");
+const toggleMenuBtn = document.getElementById("toggleMenuBtn");
+
+const toggleMenu = () => {
+  hamMenu.classList.toggle("ham-active");
+};
